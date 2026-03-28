607c6f62fa3517347be28a161973b83761635eb6
Harsh Jagtap <harshjagtap843@gmail.com>
2026-03-28 19:05:59 +0530

    update the cors origin

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -8,7 +8,10 @@ const app = express();
 
 console.log("API KEY starts with:", process.env.GEMINI_API_KEY?.substring(0, 5));
 
-app.use(cors());
+
+app.use(cors({
+  origin: '*'  
+}));
 app.use(express.json());
 app.use('/api',readmeRoutes)
 
