d107ddefb34b99e42687ac948db19394d9d2cdc2
Harsh Jagtap <harshjagtap843@gmail.com>
2026-03-28 18:46:35 +0530

    update proxy to render backend new url

--- vite.config.ts
+++ vite.config.ts
@@ -20,7 +20,7 @@ export default defineConfig(({mode}) => {
       hmr: process.env.DISABLE_HMR !== 'true',
       proxy: {
         '/api': {
-          target: 'http://localhost:3001',
+          target: 'https://readme-ai-nv5n.onrender.com/',
           changeOrigin: true,
         }
       }
